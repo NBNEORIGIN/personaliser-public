a69634de664ff0ab59d583bc1acb6b59deec3e94
NBNEORIGIN <toby@nbnesigns.com>
2025-10-25 16:52:42 +0100

    fix

--- backend/app/processors/__init__.py
+++ backend/app/processors/__init__.py
@@ -1,1 +1,7 @@
+# Import all processors to register them
+from . import text_only_v1
+from . import photo_basic_v1
+from . import uv_regular_v1
+from . import regular_stake_v1
+from . import photo_stakes_v1
 from . import photo_stakes_pdf_v1
