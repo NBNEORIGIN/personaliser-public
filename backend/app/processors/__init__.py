5e6d0c83f29d0d6b3c2af85fbc10f62472636f2a
NBNEORIGIN <toby@nbnesigns.com>
2025-10-25 17:03:00 +0100

    force-pdf

--- backend/app/processors/__init__.py
+++ backend/app/processors/__init__.py
@@ -3,5 +3,5 @@ from . import text_only_v1
 from . import photo_basic_v1
 from . import uv_regular_v1
 from . import regular_stake_v1
-from . import photo_stakes_v1
+# from . import photo_stakes_v1  # Temporarily disabled - using PDF version
 from . import photo_stakes_pdf_v1
