03f3ddbae6da8ef1d7d873aecb022fb1560e04ff
NBNEORIGIN <toby@nbnesigns.com>
2025-10-31 08:51:52 +0000

    fix-models-import-to-include-existing-models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,6 +2,21 @@
 Models package.
 """
 
+# Import existing models from parent models.py
+from ..models import (
+    Severity, QaWarning, LineField, TextLine, OrderItem,
+    Rect, PlacedRect, GenerateRequest, PreviewResponse,
+    GenerateResponse, IngestItem, IngestResponse
+)
+
+# Import new user/graphics models
 from .user import User, Graphic
 
-__all__ = ["User", "Graphic"]
+__all__ = [
+    # Existing models
+    "Severity", "QaWarning", "LineField", "TextLine", "OrderItem",
+    "Rect", "PlacedRect", "GenerateRequest", "PreviewResponse",
+    "GenerateResponse", "IngestItem", "IngestResponse",
+    # New models
+    "User", "Graphic"
+]
